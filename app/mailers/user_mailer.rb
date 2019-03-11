7524994d940bdf8a23f478e3f86c11602f9d3e1a
JulienTHP <jpelisson@hotmail.fr>
2019-03-11 17:01:43 +0100

    User Mailer added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@perma-culture.fr'
+ 
+  def welcome_email(user)
+    @user = user 
+
+    @url  = 'http://monsite.fr/login' 
+
+    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,6 +6,7 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
   
   before_create :grab_image
+  after_create :welcome_send
 
   has_one_attached :avatar
   has_many :user_categories
@@ -15,18 +17,21 @@ class User < ApplicationRecord
 
 
   validates :email, 
-	  presence: true, 
-	  uniqueness: true,
-	  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez entrer un email valide"}
+  presence: true, 
+  uniqueness: true,
+  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez entrer un email valide"}
 
-  validates :user_name, 
-    #presence: true, 
-    uniqueness: true,
-    length: { in: 3..25 }
+  # validates :user_name, 
+  #   #presence: true, 
+  #   uniqueness: true,
+  #   length: { in: 3..25 }
 
-   def grab_image
-    downloaded_image = (open('https://loremflickr.com/g/400/400/face/'))
-    self.avatar.attach(io: downloaded_image, filename: 'image.png')
-  end
+    def grab_image
+      downloaded_image = (open('https://loremflickr.com/g/400/400/face/'))
+      self.avatar.attach(io: downloaded_image, filename: 'image.png')
+    end
 
+    def welcome_send
+      UserMailer.welcome_email(self).deliver_now
+    end
   end
