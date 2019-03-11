5854c5e5e286e6dc869bda41d85efe4b4acf7dda
JulienTHP <jpelisson@hotmail.fr>
2019-03-11 13:56:57 +0100

    JSON deleted for CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,10 +30,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,10 +43,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
     redirect_to 'home/index'
@@ -61,7 +57,6 @@ class PostsController < ApplicationController
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
     end
     redirect_to 'home/index'
   end
