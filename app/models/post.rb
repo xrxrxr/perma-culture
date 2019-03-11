855d78faa3e2a665a342cb2fda77a4e0c14316bb
JulienTHP <jpelisson@hotmail.fr>
2019-03-11 12:32:07 +0100

    DB added

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+	belongs_to :writter, class_name: 'User'
+	belongs_to :category
+	#has_many :comments
+	#has_many :likes
+
+	validates :content, 
+						presence: true,	
+						length:{minimum: 25, maximum: 1000}
+						:title,
+						presence: true,
+						length:{minimum: 10, maximum: 30}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
+require 'open-uri'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
+  :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  before_create :grab_image
+  has_many :user_categories
+  has_many :categories, through: :user_categories
+  has_many :posts, foreign_key: 'writter_id'
+  #has_many :comments
+  #has_many :likes
+
+
+  validates :email, 
+  presence: true, 
+  uniqueness: true,
+  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez entrer un email valide"}
+
+  validates :user_name, 
+    #presence: true, 
+    uniqueness: true,
+    length:{minimum:6, maximum:15}
+
+   def grab_image
+    downloaded_image = (open('https://loremflickr.com/g/400/400/face/'))
+    self.avatar.attach(io: downloaded_image, filename: 'image.png')
+  end
+
+  end
