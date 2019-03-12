8a0535b8a91e6258992e92d35d68c8874d4869da
deoladed <leo.robert.mk@gmail.com>
2019-03-12 12:56:10 +0100

    Show posts by user favourite categories and latest comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :posts do
     resources :post_pictures, only: [:create]
   end
+  get '/welcome' => "posts#index", as: :user_root
 
   resources :users, only: [:show, :update, :edit, :destroy, :index] do
     resources :avatars, only: [:create]
